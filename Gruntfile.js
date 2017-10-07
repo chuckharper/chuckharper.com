6b4060a79647b03736bc215ba8e91af1eba6b32b
chuckharper <chuck@shockmediagroup.com>
2017-10-06 17:46:48 -0700

    Switch to nodejs version of site on heroku.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'public/css/screen.css' : 'sass/screen.scss',
+                    'public/css/mobile.css' : 'sass/mobile.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
