a220bfd9e5003453fe3c2f114d6a1504d03ac1df
chuckharper <chuck@shockmediagroup.com>
2017-10-09 14:24:26 -0700

    Added flicker to randomly selected images on each page.

--- public/js/inside.js
+++ public/js/inside.js
@@ -1,3 +1,28 @@
+/*
+    flickers object like it glitching
+ */
+function flicker(obj_id) {
+    if (obj_id.length == 0) return;
+    var original_opacity = $('#' + obj_id).css('opacity') * 10;
+    var loopcount = 0;
+
+    var intflick = setInterval(function() {
+        var i = Math.random();
+        var new_opacity = (Math.floor(i * (original_opacity-3)) + 3)*0.1;
+        $('#' + obj_id).css('opacity',new_opacity);
+
+        if (new_opacity == 0.6 || loopcount++ > 8) {
+            clearInterval(intflick);
+            var timeout = (Math.floor(Math.random() * 10) + 3)*1000;
+            setTimeout(function() {
+                flicker(obj_id);
+            },timeout);
+            $('#' + obj_id).css('opacity','0.6');
+        }
+
+    },100);
+}
+
 $(document).ready(function() {
     $.expr[':'].external = function(obj){
         return !obj.href.match(/^mailto\:/)
@@ -15,6 +40,18 @@ $(document).ready(function() {
     Shadowbox.init({
         displayCounter: false,
         displayNav: false
+    });
+
+    var img_count = $('img:visible').length;
+    var glitch_chance = (img_count * 0.005);
+    console.log('glitch_chance = ' + glitch_chance);
+    $('img:visible').each(function() {
+        var this_id = $(this).prop('id');
+        if (Math.random() > glitch_chance) return;
+
+        setTimeout(function() {
+            flicker(this_id);
+        },Math.floor(Math.random() * 20)*1000);
     });
 
 });
